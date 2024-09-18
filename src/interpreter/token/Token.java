f9df4e3282f1e461a3de8c9af3f96234b9260684
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-09-18 13:41:03 +0530

    added a few more identifiers and operators

--- src/interpreter/token/Token.java
+++ src/interpreter/token/Token.java
@@ -12,7 +12,8 @@ public class Token {
     private TokenType type;
     private String literal;
 
-    public Token() {}
+    public Token() {
+    }
 
     public Token(TokenType type, String literal) {
         this.type = type;
@@ -27,21 +28,23 @@ public class Token {
         IDENT("IDENT"), INT("INT"),
 
         // Operators
-        ASSIGN("ASSIGN"), PLUS("PLUS"),
+        ASSIGN("ASSIGN"), PLUS("PLUS"), MINUS("MINUS"),
+        BANG("BANG"), ASTERISK("ASTERISK"), SLASH("SLASH"),
+        LT("LT"), GT("GT"), EQ("EQ"), NOT_EQ("NOT_EQ"),
 
         // Delimiters
         COMMA("COMMA"), SEMICOLON("SEMICOLON"),
         LPARAN("("), RPARAN(")"), LBRACE("{"), RBRACE("}"),
 
         // Keywords
-        FUNCTION("FUNCTION"), LET("LET");
+        FUNCTION("FUNCTION"), LET("LET"), TRUE("TRUE"), FALSE("FALSE"),
+        IF("IF"), ELSE("ELSE"), RETURN("RETURN");
 
         private final String Literal;
 
         TokenType(String literal) {
             this.Literal = literal;
         }
-
     }
 
     private static final Map<String, TokenType> keywords = new HashMap<>();
@@ -49,6 +52,11 @@ public class Token {
     static {
         keywords.put("fn", TokenType.FUNCTION);
         keywords.put("let", TokenType.LET);
+        keywords.put("true", TokenType.TRUE);
+        keywords.put("false", TokenType.FALSE);
+        keywords.put("if", TokenType.IF);
+        keywords.put("else", TokenType.ELSE);
+        keywords.put("return", TokenType.RETURN);
     }
 
     public TokenType lookupIdentifier(final String ident) {

a14d7942a634fb489630be0124f6c2f146b2cb2a
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-09-18 16:04:11 +0530

    testing of added identifiers, and keywords

--- src/interpreter/lexer/LexerTest.java
+++ src/interpreter/lexer/LexerTest.java
@@ -14,11 +14,20 @@ public class LexerTest {
             let five = 5;
             let ten = 10;
             
-            let add = fn(x, y) {
-                x + y;
+            let add = fn(x,y) {
+            	x + y;
             };
-            
             let result = add(five, ten);
+            !-/*;
+            5 < 10 > 5;
+            
+            if (5 < 10) {
+            	return true;
+            } else {
+            	return false;
+            }
+            10 == 10;
+            10 != 9;
             """;
 
         // Expected token types and literals
@@ -57,7 +66,45 @@ public class LexerTest {
             {TokenType.IDENT, "five"},
             {TokenType.COMMA, ","},
             {TokenType.IDENT, "ten"},
-            {TokenType.RPARAN, ")"}
+            {TokenType.RPARAN, ")"},
+            {TokenType.SEMICOLON, ";"},
+            {TokenType.BANG, "!"},
+            {TokenType.MINUS, "-"},
+            {TokenType.SLASH, "/"},
+            {TokenType.ASTERISK, "*"},
+            {TokenType.SEMICOLON, ";"},
+            {TokenType.INT, "5"},
+            {TokenType.LT, "<"},
+            {TokenType.INT, "10"},
+            {TokenType.GT, ">"},
+            {TokenType.INT, "5"},
+            {TokenType.SEMICOLON, ";"},
+            {TokenType.IF, "if"},
+            {TokenType.LPARAN, "("},
+            {TokenType.INT, "5"},
+            {TokenType.LT, "<"},
+            {TokenType.INT, "10"},
+            {TokenType.RPARAN, ")"},
+            {TokenType.LBRACE, "{"},
+            {TokenType.RETURN, "return"},
+            {TokenType.TRUE, "true"},
+            {TokenType.SEMICOLON, ";"},
+            {TokenType.RBRACE, "}"},
+            {TokenType.ELSE, "else"},
+            {TokenType.LBRACE, "{"},
+            {TokenType.RETURN, "return"},
+            {TokenType.FALSE, "false"},
+            {TokenType.SEMICOLON, ";"},
+            {TokenType.RBRACE, "}"},
+            {TokenType.INT, "10"},
+            {TokenType.EQ, "=="},
+            {TokenType.INT, "10"},
+            {TokenType.SEMICOLON, ";"},
+            {TokenType.INT, "10"},
+            {TokenType.NOT_EQ, "!="},
+            {TokenType.INT, "9"},
+            {TokenType.SEMICOLON, ";"},
+            {TokenType.EOF, ""}
         };
 
         Lexer lexer = new Lexer(input);

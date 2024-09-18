fa4b3cbf6bdd06a39a2d671fd2bb218f6c4082ff
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-09-18 16:03:55 +0530

    lexing of added identifiers, and keywords

--- src/interpreter/lexer/Lexer.java
+++ src/interpreter/lexer/Lexer.java
@@ -11,19 +11,27 @@ public class Lexer {
     private final String input;
     private int position;
 
+    private String character;
     private int readPosition;
-    private char character;
 
     public Lexer(final String input) {
         this.input = input;
         readChar(input);
     }
 
+    public String peekChar(final String input) {
+        if (this.readPosition >= input.length()) {
+            return "0";
+        } else {
+            return input.substring(this.readPosition, this.readPosition + 1);
+        }
+    }
+
     public void readChar(final String input) {
         if (this.readPosition >= input.length()) {
-            this.character = 0;
+            this.character = "0";
         } else {
-            this.character = input.charAt(this.readPosition);
+            this.character = input.substring(this.readPosition, this.readPosition + 1);
         }
         this.position = this.readPosition;
         this.readPosition += 1;
@@ -33,66 +41,96 @@ public class Lexer {
         Token token;
 
         spaceEater();
-        final String character = string(this.character);
-
-        switch (character) {
+        switch (this.character) {
             case "=":
-                token = new Token(TokenType.ASSIGN, character);
-                break;
-            case ";":
-                token = new Token(TokenType.SEMICOLON, character);
-                break;
-            case "(":
-                token = new Token(TokenType.LPARAN, character);
-                break;
-            case ",":
-                token = new Token(TokenType.COMMA, character);
+                token = readTwoChar(this.character, TokenType.EQ, TokenType.ASSIGN);
                 break;
             case "+":
-                token = new Token(TokenType.PLUS, character);
+                token = new Token(TokenType.PLUS, this.character);
+                break;
+            case "-":
+                token = new Token(TokenType.MINUS, this.character);
+                break;
+            case "!":
+                token = readTwoChar(this.character, TokenType.NOT_EQ, TokenType.BANG);
+                break;
+            case "/":
+                token = new Token(TokenType.SLASH, this.character);
+                break;
+            case "*":
+                token = new Token(TokenType.ASTERISK, this.character);
+                break;
+            case "<":
+                token = new Token(TokenType.LT, this.character);
+                break;
+            case ">":
+                token = new Token(TokenType.GT, this.character);
+                break;
+            case ",":
+                token = new Token(TokenType.COMMA, this.character);
+                break;
+            case ";":
+                token = new Token(TokenType.SEMICOLON, this.character);
+                break;
+            case "(":
+                token = new Token(TokenType.LPARAN, this.character);
                 break;
             case ")":
-                token = new Token(TokenType.RPARAN, character);
+                token = new Token(TokenType.RPARAN, this.character);
                 break;
             case "{":
-                token = new Token(TokenType.LBRACE, character);
+                token = new Token(TokenType.LBRACE, this.character);
                 break;
             case "}":
-                token = new Token(TokenType.RBRACE, character);
+                token = new Token(TokenType.RBRACE, this.character);
                 break;
             case "0":
                 token = new Token();
+
                 token.setLiteral("");
                 token.setType(TokenType.EOF);
+                break;
             default:
-                if (isLetter(this.character)) {
-
-                    token = new Token();
-                    token.setLiteral(readIdentifier());
-
-                    token.setType(token.lookupIdentifier(token.getLiteral()));
-                    return token;
-                } else if (isDigit(this.character)) {
-
-                    token = new Token();
-                    token.setType(TokenType.INT);
-
-                    token.setLiteral(readNumber());
-                    return token;
-                } else {
-                    token = new Token(TokenType.ILLEGAL, character);
-                }
+                token = readDefaultToken();
+                return token;
         }
         readChar(this.input);
-
         return token;
     }
 
     private void spaceEater() {
-        while (this.character == ' ' || this.character == '\n' || this.character == '\t'
-            || this.character == '\r') {
+        while (this.character.equals(" ") || this.character.equals("\n") || this.character.equals("\t")
+            || this.character.equals("\r")) {
             readChar(this.input);
         }
+    }
+
+    private Token readTwoChar(String character, TokenType twoCharType, TokenType oneCharType) {
+        if (peekChar(this.input).equals("=")) {
+            readChar(this.input);
+            return new Token(twoCharType, character + this.character);
+        }
+        return new Token(oneCharType, character);
+    }
+
+    private Token readDefaultToken() {
+        Token token = new Token();
+
+        if (isLetter(this.character)) {
+            token.setLiteral(readIdentifier());
+            token.setType(token.lookupIdentifier(token.getLiteral()));
+            return token;
+        }
+        if (isDigit(this.character)) {
+            token.setType(TokenType.INT);
+            token.setLiteral(readNumber());
+            return token;
+        }
+        token.setType(TokenType.ILLEGAL);
+        token.setLiteral(this.character);
+
+        readChar(this.input);
+        return token;
     }
 
     private String readIdentifier() {
@@ -113,16 +151,14 @@ public class Lexer {
         return input.substring(position, this.position);
     }
 
-    private boolean isLetter(char ch) {
-        return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_';
+    private boolean isLetter(String ch) {
+        char c = ch.charAt(0);
+        return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
     }
 
-    private boolean isDigit(char ch) {
-        return '0' <= ch && ch <= '9';
-    }
-
-    private String string(char input) {
-        return Character.toString(input);
+    private boolean isDigit(String ch) {
+        char c = ch.charAt(0);
+        return '0' <= c && c <= '9';
     }
 }
 
